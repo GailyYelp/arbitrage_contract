a7172c33b6509051fdb52251c46f8f2177676dab
raindarker <raindarker@163.com>
2025-09-15 22:28:35 +0800

    feat: code

--- src/protocal/pumpfun_amm.rs
+++ src/protocal/pumpfun_amm.rs
@@ -165,7 +165,7 @@ pub fn simulate_buy_amount_by_input<'info>(
 
     // load data from pool_state
     let data = pool_state.try_borrow_data()?;
-    let lp_supply = u64::from_le_bytes(data[195..203].try_into().ok().unwrap());
+    let lp_supply = u64::from_le_bytes(data[203..211].try_into().ok().unwrap());
 
     // simulate
     let (x, y) = (base_amount, quote_amout + lp_supply);
